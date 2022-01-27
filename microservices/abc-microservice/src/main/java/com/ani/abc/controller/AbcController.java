4692dbed9ae11e409929f9d951995427fccc4f62
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-27 17:10:53 +0530

    microservice : first microservice - central refresh

--- microservices/abc-microservice/src/main/java/com/ani/abc/controller/AbcController.java
+++ microservices/abc-microservice/src/main/java/com/ani/abc/controller/AbcController.java
@@ -1,19 +1,25 @@
 package com.ani.abc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class AbcController {
 
-    @Autowired
+    @Autowired // I recommend
     private Environment env;
+
+    @Value("${some-data}")
+    private String someData;
 
     @GetMapping
     public ResponseEntity<String> sayAbc() {
-        return ResponseEntity.ok("Say Abc = "+env.getProperty("some-data"));
+        return ResponseEntity.ok("Say Abc = "+env.getProperty("some-data") + " - " +someData);
     }
 }
