0ef54e28ca34eedf28dd9123d795392fb5bafc5a
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-27 16:22:11 +0530

    microservices - config server

---
+++ microservices/central-config-server/src/test/java/com/ani/config/CentralConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
