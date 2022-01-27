cad8d4766b12dc4b9c5a0cc787c9f8a93269135e
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-27 16:21:09 +0530

    microservice : first microservice - central config

--- microservices/abc-microservice/src/main/java/com/ani/abc/controller/AbcController.java
+++ microservices/abc-microservice/src/main/java/com/ani/abc/controller/AbcController.java
@@ -1,5 +1,7 @@
 package com.ani.abc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AbcController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping
     public ResponseEntity<String> sayAbc() {
-        return ResponseEntity.ok("Say Abc");
+        return ResponseEntity.ok("Say Abc = "+env.getProperty("some-data"));
     }
 }
