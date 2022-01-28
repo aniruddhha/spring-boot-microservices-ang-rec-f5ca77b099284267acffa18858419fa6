782404bf29b9fa54f5f3a58d8087fc77e9952323
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-28 11:06:22 +0530

    microservice : inter communication : load balanced rest tenplate

--- microservices/pqr-microservice/src/main/java/com/ani/pqr/config/AppConfig.java
+++ microservices/pqr-microservice/src/main/java/com/ani/pqr/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.ani.pqr.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    @LoadBalanced // you are enabling client side load balancing by ribbon
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
