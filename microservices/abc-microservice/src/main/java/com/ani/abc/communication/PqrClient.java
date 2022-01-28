8fe476d4aeb0a1b89d5a0a9aa262aa5c0e51b7e7
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-28 12:06:41 +0530

    microservice : inter service communication : feign client

--- microservices/abc-microservice/src/main/java/com/ani/abc/communication/PqrClient.java
+++ microservices/abc-microservice/src/main/java/com/ani/abc/communication/PqrClient.java
@@ -1,11 +1,12 @@
 package com.ani.abc.communication;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "pqr", url = "http://pqr")
+@FeignClient("PQR")
 public interface PqrClient {
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String getPqr();
 }
