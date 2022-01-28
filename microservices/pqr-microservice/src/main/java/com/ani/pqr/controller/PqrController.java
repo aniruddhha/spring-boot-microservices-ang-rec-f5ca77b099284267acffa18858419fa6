46ebe163ff113a11732c2df382ddb64a4e58d44a
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-28 10:51:37 +0530

    microservice : inter communication : rest template

--- microservices/pqr-microservice/src/main/java/com/ani/pqr/controller/PqrController.java
+++ microservices/pqr-microservice/src/main/java/com/ani/pqr/controller/PqrController.java
@@ -1,14 +1,25 @@
 package com.ani.pqr.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class PqrController {
 
+    @Autowired
+    private RestTemplate template;
+
     @GetMapping
     public ResponseEntity<String> sayPqr() {
         return ResponseEntity.ok("Say Pqr");
+    }
+
+    @GetMapping("/abc")
+    public ResponseEntity<String> callAbc() {
+        ResponseEntity<String> res = template.getForEntity("http://ABC/", String.class);
+        return res;
     }
 }
